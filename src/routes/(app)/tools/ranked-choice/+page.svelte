b9c46678bfe4dcee80db225cb43c17721f868a1a
Forrest Deters <forrest.deters@gmail.com>
2024-09-14 09:13:09 -0700

    support adding new candidates

--- src/routes/(app)/tools/ranked-choice/+page.svelte
+++ src/routes/(app)/tools/ranked-choice/+page.svelte
@@ -1,20 +1,46 @@
 <script>
-  let numCandidates = 2;
-  let numBallots = 2;
+  import IconPlus from '~icons/mdi/plus';
+  import Candidate from './models/Candidate';
+
+  let newCandidateName = '';
+  let candidates = [];
+
+  const addCandidate = () => {
+    if (newCandidateName) {
+      const newCandidate = new Candidate(newCandidateName);
+      candidates = [...candidates, newCandidate];
+      newCandidateName = '';
+    }
+  };
 </script>
 
 <h1>Ranked choice voting</h1>
 
-<h2>Candidates</h2>
-<button
-  on:click={() => numCandidates += 1}
->
-  Add candidate
-</button>
+<h2 class="mt-4">Candidates</h2>
+<ul>
+  {#each candidates as candidate}
+    <li>{candidate.name}</li>
+  {/each}
+</ul>
+<form>
+  <h3>Add new candidate</h3>
+  <input
+    type="text"
+    name="new-candidate-name"
+    id="new-candidate-name"
+    aria-label="Name"
+    placeholder="Enter a name"
+    bind:value={newCandidateName}
+  />
+  <button
+    type="submit"
+    title="Add candidate"
+    on:click|preventDefault={addCandidate}
+  >
+    <IconPlus class="translate-y-0.5" />
+    <span class="sr-only">Add candidate</span>
+  </button>
+</form>
 
-<h2>Ballots</h2>
-<button
-  on:click={() => numBallots += 1}
->
-  Add ballot
-</button>
+<h2 class="mt-4">Ballots</h2>
+<button on:click={() => (numBallots += 1)}> Add ballot </button>
